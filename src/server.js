cf7f67a2523c94bd27cd88464e23fda9f6a2c369
hsattar <39243545+hsattar@users.noreply.github.com>
2022-01-14 10:50:06 +0000

    fixed db connection erorr?

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
-import productRouter from './services/products.js'
+import productRouter from './services/products/index.js'
 
 const { PORT, DB_CONNECTION } = process.env
 
