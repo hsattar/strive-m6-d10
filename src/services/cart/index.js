529095b00ce7d0f81a0cce48cfc1e69c49672b01
hsattar <39243545+hsattar@users.noreply.github.com>
2022-01-14 12:29:44 +0000

    added get, put and dlete on /:id route

--- src/services/cart/index.js
+++ src/services/cart/index.js
@@ -21,30 +21,39 @@ cartRouter.route('/')
         if (!errors.isEmpty()) return next(createHttpError(400, errors))
         const cartItem = await new CartModel(req.body)
         cartItem.save()
-        res.send(cartItem)
+        res.status(201).send(cartItem)
     } catch (error) {
         next(error)
     }
 })
 
-cartRouter.route('/:id')
+cartRouter.route('/:cartId')
 .get(async (req, res, next) => {
     try {
-        res.send('GET ID')
+        if (req.params.cartId.length !== 24) return next(createHttpError(400, 'Invalid ID'))
+        const cartItem = await CartModel.findById(req.params.cartId)
+        if (!cartItem) return next(createHttpError(404, 'We couldn\'t find the item you were looking for'))
+        res.send(cartItem)
     } catch (error) {
         next(error)
     }
 })
 .put(async (req, res, next) => {
     try {
-        res.send('PUT')
+        if (req.params.cartId.length !== 24) return next(createHttpError(400, 'Invalid ID'))
+        const updatedItem = await CartModel.findByIdAndUpdate(req.params.cartId, req.body, { new: true, runValidators: true })
+        if (!updatedItem) return next(createHttpError(404, 'We couldn\'t update the item because the id is wrong'))
+        res.send(updatedItem)
     } catch (error) {
         next(error)
     }
 })
 .delete(async (req, res, next) => {
     try {
-        res.send('DELETE')
+        if (req.params.cartId.length !== 24) return next(createHttpError(400, 'Invalid ID'))
+        const result = await CartModel.findByIdAndDelete(req.params.cartId)
+        if (!result) return next(createHttpError(404, 'We couldn\'t delete the item because the id is wrong'))
+        res.sendStatus(204)
     } catch (error) {
         next(error)
     }
